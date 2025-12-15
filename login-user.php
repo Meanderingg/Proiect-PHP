e7c1609f24a10afbb4a98d63ff82016b0fb2a54a
Meanderingg <149524587+Meanderingg@users.noreply.github.com>
2025-12-15 15:03:14 +0200

    login si creare user

---
+++ login-user.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login User</title>
+    <link rel="stylesheet" href="styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container mt-5">
+        <h1 class="mb-4">Login User</h1>
+        <form action="login-user.php" method="POST">
+            <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" class="form-control" id="email" name="email" required>
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control" id="password" name="password" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Login</button>
+        </form>
+    </div>
+
+    <?php
+    require_once './Database.php';
+
+    if (isset($_POST['email']) || isset($_POST['password'])) {
+
+    try {
+        $pdo = Database::getInstance()->getConnection();
+        echo "✅ Connection successful!<br>";
+    } catch (PDOException $e) {
+        die("❌ Connection failed: " . $e->getMessage());
+    }
+
+    // Example select
+    try {
+        $sql = "SELECT * FROM users WHERE email = :email";
+        
+        $stmt = $pdo->prepare($sql);
+
+        // Sample data
+        $data = [
+            'email'    => $_POST['email']
+        ];
+
+        $stmt->execute($data);
+        $user = $stmt->fetch();
+
+        var_dump($user);
+
+        if ($user && password_verify($_POST['password'], $user['password'])) {
+
+            echo "✅ Login successful! Welcome, " . htmlspecialchars($user['name']) . ".<br>";
+            header('Location: ./homepage.php');
+        } else {
+            echo "❌ Invalid email or password.<br>";
+        }
+    } catch (PDOException $e) {
+        echo "❌ Query failed: " . $e->getMessage();
+    }
+    }
+    ?>
