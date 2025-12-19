b0b5ad28b3b8c7ec54bfda7ccff65d234ea08f5c
T S <t@t.t>
2025-12-19 18:36:17 +0200

    new file:   TODO.md 	modified:   homepage.php, added an auto-updating list of articles

---
+++ TODO.md
@@ -0,0 +1,8 @@
+# TODO
+
+1. Make the homepage display all the articles
+2. Create a header with links to login and homepage
+3. Create admin user, roles, make the reading page available only to logged in users
+4. Create the roles administration page for admins
+5. Create the article editing and submitting page for editors
+6. Make article submitting only available for authors
--- homepage.php
+++ homepage.php
@@ -1,4 +1,47 @@
 <!DOCTYPE html>
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require_once './Database.php';
+try {
+    $pdo = Database::getInstance()->getConnection();
+
+    // Fetch all the articles
+    $sql = "SELECT article_id, title FROM articles" ;
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+    
+    $record = $stmt->fetchALL(PDO::FETCH_ASSOC); //Return each row as an associative array, using column names as keys
+    if (!$record) {
+        die("No article found ");
+    }
+    //var_dump($record);
+
+    // Declare variables to hold the fetched data
+
+    /*
+    $title = $record['title'];
+    $id = $record['article_id'];
+
+    //de facut lista cu id-urile si titlul
+    
+     */
+    /*
+    echo ("<h1> " . htmlspecialchars($title) . "</h1>
+          <h3>" . htmlspecialchars($date) . "</h3>
+          <p>" . nl2br(htmlspecialchars($contents)) . "</p>"
+        );
+     
+    */
+} catch (PDOException $e) {
+    die(" Connection failed: " . $e->getMessage());
+}
+
+
+?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -8,4 +51,13 @@
 
 <body>
     <h1>SUCCESS</h1>
+    <ul>
+    <?php foreach ($record as $record): ?>
+        <li>
+            <a href="read-articles.php?id=<?= htmlspecialchars($record['article_id']) ?>">
+                <?= htmlspecialchars($record['title']) ?>
+            </a>
+        </li>
+    <?php endforeach; ?>
+</ul>
 </body>
