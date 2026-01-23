325c063f0da89be9a73b25144935d148f8633ab8
T S <t@t.t>
2026-01-23 18:35:13 +0200

    The article post works only for authors and sends the author id to the database

--- TODO.md
+++ TODO.md
@@ -1,3 +1,2 @@
 # TODO
 
--- When an auhor publishes an aticle, also got it's id and send it to the database
--- create-articles.php
+++ create-articles.php
@@ -1,32 +1,43 @@
 <?php
-require_once './Database.php';
 
-if (isset($_POST['title']) || isset($_POST['contents'])) {
+require_once './Database.php';
+include './operatii_db.php';
+
+session_start();
+
+if(!isset($_SESSION['author'])){
+            header('Location: ./homepage.php');
+            exit;
+}
+
+if (isset($_POST['title']) && isset($_POST['contents'])) {
 
 try {
     $pdo = Database::getInstance()->getConnection();
-    echo " Connection successful!<br>";
+    //echo " Connection successful!<br>";
 } catch (PDOException $e) {
     die(" Connection failed: " . $e->getMessage());
 }
 
 // Example insert
 try {
-    $sql = "INSERT INTO articles (title, contents) 
-            VALUES (:title,:contents)";
+    $sql = "INSERT INTO articles (title, contents, author_id) 
+            VALUES (:title,:contents,:author_id)";
     
     $stmt = $pdo->prepare($sql);
 
     // Sample data
     $data = [
         'title'             => $_POST['title'] ?? 'Sample Title',
-        'contents'       => $_POST['contents'] ?? 'Sample Description'
+        'contents'       => $_POST['contents'] ?? 'Sample Description',
+        'author_id'       => $_SESSION['author'] ?? '0'
     ];
 
     $stmt->execute($data);
 
     //Redirect user to /read-stiri.php?id=lastInsertId() -- de schimbat din homepage.php
-    header("Location: read-articles.php?id=" . $pdo->lastInsertId());
+    header("Location: homepage.php");
+    exit;
 
 } catch (PDOException $e) {
     echo " Insert failed: " . $e->getMessage();
